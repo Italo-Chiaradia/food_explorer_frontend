388245f19861df200f57acbc45a99ba7799ac454
Italo Chiaradia <italocoelho050704@gmail.com>
2024-08-23 10:18:49 -0300

    add Header component desktop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,31 +1,77 @@
-import { Container, Menu, Logo, Orders } from "./styles";
-
+import { Input } from "../Input";
+import { Button } from "../Button";
+import { FiLogOut } from "react-icons/fi";
 import { PiReceipt } from "react-icons/pi";
 import { RiMenuFill } from "react-icons/ri";
+import { RiSearchLine } from "react-icons/ri";
+import { useMediaQuery } from "react-responsive"; 
 import { BsFillHexagonFill } from "react-icons/bs";
+import BREAKPOINTS, {formatDeviceBreakpoints} from "../../utils/deviceBreakpoints";
+import { Container, Menu, Logo, Orders, LogOutButton } from "./styles";
 
 export function Header({onOpenMenu, user}) {
   const role = user.role;
-  
+  const isDesktop = useMediaQuery({minWidth: formatDeviceBreakpoints(BREAKPOINTS.sm)})
   return (
     <Container>
-      <Menu onClick={onOpenMenu}>
-        <RiMenuFill/>
-      </Menu>
-      <Logo data-role={role}>
-        <BsFillHexagonFill /> 
-        <h1>food explorer</h1>
+      <div>
+        
         {
-          role === "admin" && <span>admin</span>
+          !isDesktop &&
+          <Menu onClick={onOpenMenu}>
+            <RiMenuFill/>
+          </Menu>
         }
-      </Logo>
-      {
-        role != "admin" && 
-        <Orders>
-          <PiReceipt/> 
-          <span>0</span>
-        </Orders>
-      }
+        <Logo data-role={role}>
+          <div>
+            <BsFillHexagonFill /> 
+            <h1>food explorer</h1>
+          </div>
+          {
+            role === "admin" && <span>admin</span>
+          }
+        </Logo>
+        {
+          isDesktop &&
+          <div className="search-bar">
+            <Input 
+              icon={RiSearchLine}
+              type="text"
+              placeholder="Busque por pratos ou ingredientes"
+            />
+          </div>
+        }
+        {
+          role != "admin" && ( 
+          !isDesktop ? (
+            <Orders>
+              <PiReceipt />
+              <span>0</span>
+            </Orders>
+          ) : (
+            <Button className="btn">
+              <PiReceipt />
+              Pedidos
+              <span>(0)</span>
+            </Button>
+          ))
+        }
+        {
+          role == "admin" && ( 
+            isDesktop && (
+              <Button className="btn">
+                Novo prato
+              </Button>  
+            )
+          )
+        }
+        {
+          isDesktop && 
+          <LogOutButton>
+            <FiLogOut/>
+          </LogOutButton>
+        }
+      </div>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,13 +1,47 @@
 import styled from "styled-components";
+import BREAKPOINTS from "../../utils/deviceBreakpoints";
 
 export const Container = styled.header`
   grid-area: header;
   height: 11.4rem;
   background-color: ${({theme}) => theme.COLORS.DARK.DARK700};
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-  padding: 0 2.8rem 2.4rem;
+  > div {
+    max-width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: 0 2.8rem 2.4rem;
+  
+    @media (min-width: ${BREAKPOINTS.sm}) {
+      max-width: 90vw;
+      margin: auto;
+      align-items: center;
+      padding: 0;
+      gap: 2.4rem;
+      .search-bar {
+        flex: 1;
+      }
+      > button.btn {
+        width: fit-content;
+      }
+    }
+    @media (min-width: ${BREAKPOINTS.md}) {
+      max-width: 80vw;
+      gap: 3.2rem;
+      > button.btn {
+        width: 21.6rem;
+      }
+    }
+    @media (min-width: ${BREAKPOINTS.lg}) {
+      .search-bar label > div {
+        justify-content: center;
+        > input {
+          width: 50%;
+        }
+      }
+    }
+  }
 `;
 
 export const Menu = styled.button`
@@ -27,26 +61,38 @@ export const Menu = styled.button`
 
 export const Logo = styled.div`
   display: flex;
-  &[data-role="admin"] {
-    flex: 1;
-    justify-content: center;
-    align-items: center;
+  align-items: center;
+  gap: .8rem;
+  div {
+    display: flex;
+    align-items: end;
+    
+    gap: .8rem;
+  }
+  div > h1 {
+    font-size: 2.1rem;
+    font-weight: 700;
+  }
+  div > svg {
+    font-size: 2.5rem;
+    color: ${({theme}) => theme.COLORS.TINTS.CAKE100};
   }
   > span {
     font-size: 1.2rem;
     color: ${({theme}) => theme.COLORS.TINTS.CAKE200};
   }
-  align-items: center;
-  gap: .8rem;
-  > svg {
-    font-size: 2.5rem;
-    color: ${({theme}) => theme.COLORS.TINTS.CAKE100};
+  &[data-role="admin"] {
+    flex: 1;
+    justify-content: center;
   }
-  > h1 {
-    font-size: 2.1rem;
-    font-weight: 700;
-  }
-
+  @media (min-width: ${BREAKPOINTS.sm}) {
+    flex-direction: column;
+    align-items: flex-end;
+    gap: .2rem;
+    &[data-role="admin"] {
+      flex: none;
+    }
+  } 
 `;
 
 export const Orders = styled.button`
@@ -76,6 +122,20 @@ export const Orders = styled.button`
     filter: brightness(.5);
   }
   > svg:hover + span {
+    filter: brightness(.5);
+  }
+`;
+
+export const LogOutButton = styled.button`
+  border: none;
+  background-color: inherit;
+  cursor: pointer;
+  transition: filter .2s;
+  > svg {
+    color: ${({theme}) => theme.COLORS.LIGHT.LIGHT100};
+    font-size: 3.2rem;
+  }
+  > svg:hover {
     filter: brightness(.5);
   }
 `;
