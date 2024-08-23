5cd597df98566c85077830b83afd26b9f448edac
Italo Chiaradia <italocoelho050704@gmail.com>
2024-08-23 10:40:06 -0300

    update Footer component mobile desktop

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,34 +1,50 @@
 import styled from "styled-components";
 import BREAKPOINTS from "../../utils/deviceBreakpoints";
+
 export const Container = styled.footer`
   grid-area: footer;
-  background-color: ${({theme}) => theme.COLORS.DARK.DARK700};
+  height: 7.7rem;
+  background-color: ${({ theme }) => theme.COLORS.DARK.DARK700};
   display: grid;
   place-items: center;
-  @media (max-width: ${BREAKPOINTS.xs}) {
-    > div {
-      flex-direction: column;
-    }
-  }
+
   > div {
     width: 80%;
     display: flex;
     justify-content: space-between;
     align-items: center;
     gap: 1.6rem;
+    flex-direction: column;
+
     > span {
-      font-size: 1.2rem;
+      font-size: 1.4rem;
     }
+
     > span:first-child {
       display: flex;
       align-items: center;
       font-size: 1.57rem;
       font-weight: 700;
-      color: ${({theme}) => theme.COLORS.LIGHT.LIGHT700};
-      gap: .7rem;
+      color: ${({ theme }) => theme.COLORS.LIGHT.LIGHT700};
+      gap: 0.7rem;
+
       > svg {
-        width: 2.2rem;
         font-size: 1.8rem;
+      }
+    }
+  }
+
+  @media (min-width: ${BREAKPOINTS.xs}) {
+    > div {
+      flex-direction: row;
+    }
+
+    > div > span:first-child {
+      font-size: 2.4rem;
+      gap: 1.1rem;
+
+      > svg {
+        font-size: 2.9rem;
       }
     }
   }
