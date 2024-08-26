056d2d9ea92a7f0469de38c23f6b3aeb8a8344d1
Italo Chiaradia <italocoelho050704@gmail.com>
2024-08-26 18:26:55 -0300

    add Card component desktop

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,35 +1,49 @@
 import Edit from "../../assets/svg/edit.svg";
 import Favorite from "../../assets/svg/favorite.svg";
-import maracuja from "../../assets/maracuja.png";
 import NotFavorite from "../../assets/svg/not-favorite.svg";
+import torrada from "../../assets/torrada.png";
 import { Button } from "../Button";
 import { Counter } from "../Counter";
 import { useState } from "react";
-import { Container, Image, Title, Price, TopCornerButton } from "./styles";
+import { Container, Footer, Image, Title, Price, TopCornerButton, Description } from "./styles";
+
+import {useMediaQuery} from "react-responsive";
+import BREAKPOINTS, {formatDeviceBreakpoints} from "../../utils/deviceBreakpoints";
 
 export function Card({user, data}) {
+
+  const isDesktop = useMediaQuery({
+    minWidth: formatDeviceBreakpoints(BREAKPOINTS.sm)
+  });
 
   const [isFavorite, setIsFavorite] = useState(false);
 
   return (
     <Container>
-      <Image src={maracuja} alt="Torrada de parma" />
+      <Image src={torrada} alt="Torrada de parma" />
 
       <Title>
         {data.title + " >"}
       </Title>
+      {
+        isDesktop && (
+          <Description>
+            {data.description}
+          </Description>
+        )
+      }
       <Price>
         {"R$ " + data.price}
       </Price>
 
       {
         user.role !== "admin" && (
-          <div>
+          <Footer>
             <Counter/>
             <Button>
               incluir
             </Button>
-          </div>
+          </Footer>
         )
       }
       
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,25 +1,50 @@
 import styled from "styled-components";
+import BREAKPOINTS from "../../utils/deviceBreakpoints";
 
 export const Container = styled.div`
   position: relative;
   width: 21rem;
   min-height: 29.2rem;
   padding: 2.4rem;
+  margin: 2rem;
+  background-color: ${({theme}) => theme.COLORS.DARK.DARK200};
+  border-radius:  .8rem;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 1.2rem;
-  background-color: ${({theme}) => theme.COLORS.DARK.DARK200};
-  border-radius:  .8rem;
-  margin: 2rem;
-  > div {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 1.6rem;
+
+  @media (min-width: ${BREAKPOINTS.sm}) {
+    width: 30.4rem;
+    min-height: 46.2rem;
+    gap: 1.5rem;
+    padding: 2.4rem;
+  }
+`;
+
+export const Footer = styled.footer`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  > button {
+    padding-block: .4rem;
+  }
+  @media (min-width: ${BREAKPOINTS.sm}) {
+    flex-direction: row;
+    > div {
+      > span {
+        font-size: 2rem;
+        font-weight: 700;
+        line-height: 1.6;
+      }
+      flex: 1;
+    }
     > button {
-      padding-block: .4rem;
+      flex: 1;
+      padding-block: 1.2rem;
     }
   }
 `;
@@ -27,6 +52,10 @@ export const Container = styled.div`
 export const Image = styled.img`
   width: 8.8rem;
   height: 8.8rem;
+  @media (min-width: ${BREAKPOINTS.sm}) {
+    width: 17.6rem;
+    height: 17.6rem;
+  }
 `;
 
 export const Title = styled.h1`
@@ -34,11 +63,27 @@ export const Title = styled.h1`
   font-size: 1.4rem;
   font-weight: 500;
   line-height: 2.4rem;
+  @media (min-width: ${BREAKPOINTS.sm}) {
+    font-size: 2.4rem;
+    font-weight: 600;
+    line-height: 1.4;
+  }
+`;
+
+export const Description = styled.span`
+  font-size: 1.4rem;
+  text-align: center;
+  line-height: 1.6;
+  color: ${({theme}) => theme.COLORS.LIGHT.LIGHT400}
 `;
 
 export const Price = styled.span`
   font-size: 1.6rem;
-  color: ${({theme}) => theme.COLORS.TINTS.CAKE200}
+  color: ${({theme}) => theme.COLORS.TINTS.CAKE200};
+  @media (min-width: ${BREAKPOINTS.sm}) {
+    font-size: 3.2rem;
+    line-height: 1.6;
+  }
 `;
 
 export const TopCornerButton = styled.button`
