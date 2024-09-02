7fd3cb62847d37849db9ae1f1f468394d2599099
Italo Chiaradia <italocoelho050704@gmail.com>
2024-09-02 11:52:57 -0300

    add Details page desktop

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,16 +1,20 @@
+import Dish from "../../assets/torrada.png";
 import { Tag } from "../../components/Tag";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Counter } from "../../components/Counter";
+import BackArrow from "../../assets/svg/back-arrow.svg";
 import { useState } from "react";
 import { SideMenu } from "../../components/SideMenu";
+import { PiReceipt } from "react-icons/pi";
 import { Container, ScrollContent, Content } from "./styles.js";
-import BackArrow from "../../assets/svg/back-arrow.svg";
-import Dish from "../../assets/torrada.png";
 
-
+import { useMediaQuery } from "react-responsive"; 
+import BREAKPOINTS, {formatDeviceBreakpoints} from "../../utils/deviceBreakpoints";
 export function Details() {
-  const user = {"role":"admin"}
+  const isDesktop = useMediaQuery({minWidth: formatDeviceBreakpoints(BREAKPOINTS.md)});
+  const user = {"role":""}
   const [menu, setMenu] = useState(false);
   const tags = ["alface", "cebola", "pão naan", "pepino", "rabanete", "tomate"];
   return (
@@ -21,28 +25,56 @@ export function Details() {
       />
       <ScrollContent>
         <Content>
-          <a href="">
-            <img src={BackArrow} />
-            voltar
-          </a>
-          <img src={Dish} />
-          <div>
-            <h2>Salada Ravanello</h2>
-            <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.</p>
-            <div className="tags-container">
+          <div className="dish-image">  
+            <a href="">
+              <img src={BackArrow} />
+              voltar
+            </a>
+            <img src={Dish} />
+          </div>
+
+          <div className="dish-content">
+            <div className="dish-info">
+              <h2>Salada Ravanello</h2>
+              <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.</p>
+              <div className="tags-container">
+                {
+                  tags && tags.map((tag, index) => {
+                    return <Tag 
+                      key={String(index)}
+                      title={tag}
+                    />
+                  })
+                }
+              </div>
+            </div>
+            <div className="dish-count">
               {
-                tags && tags.map((tag, index) => {
-                  return <Tag 
-                    key={String(index)}
-                    title={tag}
-                  />
-                })
+                user.role === "admin" ? (
+                  <Button>
+                    Editar prato
+                  </Button>
+                ) : (
+                  <>
+                    <Counter/>
+                    <Button className="order-btn">
+                      {
+                        !isDesktop ? (
+                          <>
+                            <PiReceipt/>
+                            pedir • 
+                          </>
+                        ) : (
+                          <span>incluir • </span>
+                        )
+                      }
+                      <span>R$ 25,00</span>
+                    </Button>
+                  </>
+                )
               }
             </div>
           </div>
-          <Button>
-            Editar prato
-          </Button>
         </Content>
         <Footer/>
       </ScrollContent>
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import BREAKPOINTS from "../../utils/deviceBreakpoints";
 
 export const Container = styled.div`
   width: 100%;
@@ -25,50 +26,122 @@ export const ScrollContent = styled.div`
 export const Content = styled.div`
   max-width: 31.6rem;
   margin-top: 1.6rem;
+  margin-bottom: 5rem;
   margin-inline: auto;
-  margin-bottom: 3.3rem;
-  > a {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    font-family: "Poppins", sans-serif;
-    font-size: 2.4rem;
-    line-height: 1.4;
-    font-weight: 500;
-    > img {
-      width: .9rem;
-    }
-  }
-  > img {
-    max-width: 100%;
-    height: auto;
-    margin-block: 1.6rem;
-    margin-inline: auto;
-    display: block;
-  }
-  > div {
-    font-family: "Poppins", sans-serif;
-    text-align: center;
-    display:flex;
-    flex-direction: column;
-    gap: 2.4rem;
-    > h2 {
-      font-size: 2.7rem;
-      line-height: 1.4; 
-      font-weight: 500;
-    }
-    > p {
-      font-size: 1.6rem;
-      line-height: 1.4;
-    }
-    > .tags-container {
+
+  display: flex;
+  flex-direction: column;
+
+  > div.dish-image {
+    > a {
       display: flex;
-      gap: 2.4rem;
-      justify-content: center;
-      flex-wrap: wrap;
+      align-items: center;
+      gap: 1rem;
+      font-family: "Poppins", sans-serif;
+      font-size: 2.4rem;
+      line-height: 1.4;
+      font-weight: 500;
+      > img {
+        width: .9rem;
+      }
+    }
+    > img {
+      max-width: 100%;
+      height: auto;
+      margin-block: 1.6rem;
+      margin-inline: auto;
+      display: block;
     }
   }
-  > button {
-    margin-top: 4.8rem;
+  > div.dish-content {
+    display: flex;
+    flex-direction: column;
+    > div.dish-info {
+      font-family: "Poppins", sans-serif;
+      text-align: center;
+      display:flex;
+      flex-direction: column;
+      gap: 2.4rem;
+      > h2 {
+        font-size: 2.7rem;
+        line-height: 1.4; 
+        font-weight: 500;
+      }
+      > p {
+        font-size: 1.6rem;
+        line-height: 1.4;
+      }
+      > .tags-container {
+        display: flex;
+        gap: 2.4rem;
+        justify-content: center;
+        flex-wrap: wrap;
+      }
+    }
+    > div.dish-count {
+      margin-top: 4.8rem;
+      display: flex;
+      > div {
+        flex: 1;
+      }
+      > button.order-btn {
+        flex: 1;
+        font-size: 1.2rem;
+        > svg {
+          font-size: 2rem;
+        }
+      }
+    }
+  }
+
+  @media (min-width: ${BREAKPOINTS.md}) {
+    max-width: min(90vw, 112rem);
+    height: fit-content;
+    align-items: center;
+    margin-top: 2.4rem;
+    flex-direction: row;
+    gap: 4.7rem;
+
+    > div.dish-image {
+      > a {
+        font-weight: 700;
+        > img {
+          width: 1.2rem;
+        }
+      }
+      > img {
+        max-width: 39rem;
+        height: auto;
+        margin-top: 4.2rem;
+      }
+    }
+
+    > div.dish-content {
+      > div.dish-info {
+        align-items: start;
+        text-align: start;
+        > .tags-container {
+          justify-content: start;
+        }
+        h2 {
+          font-size: 4rem;
+          font-weight: 500;
+        }
+        p {
+          font-size: 2.4rem;
+        }
+      }
+      > div.dish-count {
+        gap: 3.3rem;
+        > button.order-btn {
+          font-size: 1.4rem;
+        }
+        > div, button.order-btn, button {
+          width: fit-content;
+          flex: none;
+          justify-self: start;
+        }
+      } 
+    }
   }
 `;
